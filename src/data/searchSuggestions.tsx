17702c7c1fdd5443bd0965b6912fc6cc1fa18dc2
Mohammed 'Mo' Mulazada <mohammed.mulazada@debijenkorf.nl>
2023-05-24 13:45:30 +0200

    add formatting

--- src/data/searchSuggestions.tsx
+++ src/data/searchSuggestions.tsx
@@ -1,48 +1,48 @@
-import { Suggestion } from '@/types';
+import { Suggestion } from "@/types";
 
 export const suggestions: Suggestion[] = [
   {
-    searchterm: 'heren truien',
+    searchterm: "heren truien",
     nrResults: 1100,
   },
   {
-    searchterm: 'dames truien',
+    searchterm: "dames truien",
     nrResults: 1501,
   },
   {
-    searchterm: 'kenzo trui',
+    searchterm: "kenzo trui",
     nrResults: 62,
   },
   {
-    searchterm: 'kenzo trui dames',
+    searchterm: "kenzo trui dames",
     nrResults: 21,
   },
   {
-    searchterm: 'kenzo trui heren',
+    searchterm: "kenzo trui heren",
     nrResults: 12,
   },
   {
-    searchterm: 'armani truien',
+    searchterm: "armani truien",
     nrResults: 39,
   },
   {
-    searchterm: 'daily paper trui',
+    searchterm: "daily paper trui",
     nrResults: 2,
   },
   {
-    searchterm: 'calvin klein trui',
+    searchterm: "calvin klein trui",
     nrResults: 54,
   },
   {
-    searchterm: 'calvin klein trui heren rood',
+    searchterm: "calvin klein trui heren rood",
     nrResults: 40,
   },
   {
-    searchterm: 'calvin klein trui heren blauw',
+    searchterm: "calvin klein trui heren blauw",
     nrResults: 50,
   },
   {
-    searchterm: 'calvin klein trui heren oranje',
+    searchterm: "calvin klein trui heren oranje",
     nrResults: 42,
   },
 ];
